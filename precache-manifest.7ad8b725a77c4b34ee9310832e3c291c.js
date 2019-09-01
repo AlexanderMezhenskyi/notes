8854669cfa52f4cd57cc48c0824ff3918d88e32c
AlexanderMezhenskyi <alexmezhenskyi@gmail.com>
2019-09-01 09:04:37 +0300

    Updates

---
+++ precache-manifest.7ad8b725a77c4b34ee9310832e3c291c.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d26c95d3bf1efb93df7b03d590357c32",
+    "url": "/notes/index.html"
+  },
+  {
+    "revision": "4515ccc76a9e48d684c9",
+    "url": "/notes/static/css/main.a38d7ec2.chunk.css"
+  },
+  {
+    "revision": "a210d0169bc09f59f013",
+    "url": "/notes/static/js/2.f76a1a8f.chunk.js"
+  },
+  {
+    "revision": "4515ccc76a9e48d684c9",
+    "url": "/notes/static/js/main.d1de0b34.chunk.js"
+  },
+  {
+    "revision": "6e6216fc332e034d2c65",
+    "url": "/notes/static/js/runtime~main.bffad4ac.js"
+  }
+]);
